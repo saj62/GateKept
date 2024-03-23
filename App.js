5b6503c92c20a8fe237fa011bba11c7096ac706c
saj62 <sarannjac@gmail.com>
2024-03-23 16:43:15 -0500

    tsconfig.json

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Layout from './app/_layout';
 
 export default function App() {
   return (
